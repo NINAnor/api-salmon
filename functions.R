9c8b1f8cc3408b759ff22aed70ff8c675541a0d2
Benjamin Cretois <benjamin.cretois@nina.no>
2023-01-18 16:01:31 +0100

    Switch to API fully in R

---
+++ functions.R
@@ -0,0 +1,65 @@
+library(DBI)
+library(assertthat)
+
+# Function to connect to the fish database without any effort
+fiskdatabasen_connect <- function(user, password){
+  
+  con <- DBI::dbConnect(odbc(),
+                        Driver = "SQL Server", 
+                        server = "ninsql07.nina.no",
+                        database = "Fiskedatabasen",
+                        uid = user,
+                        pwd= password)
+  return(con)
+}
+
+########################
+# SOME UTILS FUNCTIONS #
+########################
+get_columns <- function(con, table){
+  columns <- dbListFields(con, table)
+  return(columns)
+}
+
+transform_to_string <- function(column) {
+  return(paste0("\"", paste(column, collapse = "\", \""), "\""))
+}
+
+#############################################
+# QUERY THE TABLE -> ABSTRACT THE SQL QUERY #
+#############################################
+return_columns <- function(columns){
+  
+  # If some columns are specified convert the string
+  if (!is.na(columns)){
+    columns <- transform_to_string(columns)
+  }
+  # If not, just select all
+  else if (is.na(columns)){
+    columns = "*"
+  }
+  else{
+    print("Please select existing columns")
+  }
+}
+
+query_table <- function(con, table, columns = NA, condition=NA){
+  
+  columns <- return_columns(columns)
+  
+  # If no conditions is specified, select everything
+  if (is.na(condition)){
+    result <- dbGetQuery(con, paste('SELECT', columns, 'FROM', table))
+  }
+  # If conditions are specified filter with regards to the condition
+  else if (is.string(condition)){
+    result <- dbGetQuery(con, paste('SELECT', columns, 'FROM', table, 'WHERE', condition))
+  }
+  else{
+    print("This is an invalid condition. Please note that the function can only support 
+          a single condition.")
+  }
+  # Return the results of the query
+  return(result)
+}
+
